cc39845ff9f33b32fa0d330d455f077f55e3ecd8
tom <testotomasyonmuhendisi@gmail.com>
2023-01-29 08:46:47 -0600

    ders-17 ddt ile farklı senaryo türleri

--- tests/test_arama.py
+++ tests/test_arama.py
@@ -24,9 +24,14 @@ class TestArama(unittest.TestCase):
 
     @data(*ExcelYardimcisi.excel_listeler_listesine_cevir("./testdata/arama.xlsx", "Sheet1"))
     @unpack
-    def test_arama_uyari_verir(self, kelime, beklenen_mesaj):
+    def test_arama(self, senaryoturu, kelime, beklenen_mesaj):
         self.driver.get("https://demowebshop.tricentis.com/")
         arama = AramaSayfasi(self.driver)
         arama.arama_yap(kelime)
-        mesaj = arama.arama_uyari_mesajini_ver()
-        assert mesaj == beklenen_mesaj
+        if senaryoturu.lower() == "negatif":
+            mesaj = arama.arama_uyari_mesajini_ver()
+            assert mesaj == beklenen_mesaj
+        elif senaryoturu.lower() == "pozitif":
+            urun_isimleri = arama.aranan_urun_isimlerini_liste_ver()
+            for isim in urun_isimleri:
+                assert kelime.lower() in isim.lower()
